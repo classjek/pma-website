54373665b9e740f819c1a34275c3f54292dfa39b
classjek <jekoniak13@gmail.com>
2023-07-06 17:09:35 -0700

    Email Submission Component
    Need to ask where to link this to

---
+++ src/components/EmailSub.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function EmailSub() {
+    const[email, setEmail] = useState("");
+
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+        try{
+            console.log(email)
+        } catch(error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <div className= 'bg-pma-dark-orange h-36 flex justify-center items-center'>
+            <h1 className=' text-white font-canela px-1 text-2xl'>Stay in the loop! App coming soon.</h1>
+            <form onSubmit={handleSubmit} className='m-4 flex'>
+                <input className=' rounded-l-lg px-4 mr-0' 
+                    type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                    placeholder="amy@gmail.com"
+                    required
+                />
+                <button type="submit" className='bg-pma-orange px-8 rounded-r-lg text-gray-800 p-2 font-bold uppercase'>SUBMIT</button>
+            </form>
+        </div>
+    );
+}
+
+export default EmailSub
