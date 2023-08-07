83a64268517061dc4b4ded184559f1f3eba18d94
classjek <jekoniak13@gmail.com>
2023-08-07 14:28:57 -0700

    install line-clamp, get rid of loading error

--- src/components/widgets/StoryWidget.js
+++ src/components/widgets/StoryWidget.js
@@ -6,13 +6,11 @@ const StoryWidget = () => {
 const [stories, setStories] = useState([]);
 
 //can probably remove this
-const [loading, setLoading] = useState(false);
 
 const navigate = useNavigate();
 
 useEffect(() => {
   const fetchStories = async () => {
-    setLoading(true);
     try {
       const res = await axios.post('http://localhost:3001/stories', {
         numStories: 3,
@@ -25,7 +23,6 @@ useEffect(() => {
     } catch (error) {
       console.error('Failed to fetch stories:', error);
     } finally {
-      setLoading(false);
     }
   };
 
@@ -34,20 +31,20 @@ useEffect(() => {
 
   return (
     <div className='px-10'>
-      <div className='flex justify-around'>
+      <div className='flex justify-around px-24'>
         {stories.length > 0 ? (
           stories.map((s) => (
             <div key={s._id} className='flex flex-col items-center'>
               <img src={s.imageUrls.length > 0 ? s.imageUrls[0] : null} alt="" 
-                className=' h-48 w-auto'
+                className=' h-64 w-auto'
                 onLoad={(e) => {
                   // When the image is loaded, set the width of the container div
                   e.target.parentNode.style.width = `${e.target.offsetWidth}px`;
                 }}
               />
-              <div className='bg-pma-orange w-full'>
+              <div className='bg-pma-orange w-full overflow-hidden'>
                 <button onClick={ ()=> navigate(`/stories/${s._id}`)}>
-                  <h1 className='font-canela text-sm p-1'>{s.text?.en}</h1>
+                  <h1 className='font-canela text-sm line-clamp-3 overflow-x-auto whitespace-normal break-words'>{s.text?.en}</h1>
                 </button>
                 <p className='font-avenir text-xs text-gray-600'>July 28th, 2023</p>
               </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,8 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
 }
 
