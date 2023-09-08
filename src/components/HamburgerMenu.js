9c417bccb82d0a2995406249e9c8cf90352ef190
classjek <jekoniak13@gmail.com>
2023-09-08 12:48:59 -0700

    Style hamburger menu

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -15,22 +15,24 @@ export const HamburgerMenu = () => {
         </button>
 
       {isOpen && (
-        <ul className="flex flex-col bg-gray-400 text-gray-700 shadow-lg rounded-lg m-5 w-1/4 fixed top-0 right-0 h-auto z-10 p-5"Custom>
+        <ul className="flex flex-col bg-white text-gray-900 border-2 border-gray-300 shadow-md rounded-lg m-5 w-1/4 fixed top-0 right-0 h-auto z-10 pl-5 pb-5"Custom>
           <button
-            className="p-2 self-end"
+            className="pt-2 pr-1 self-end"
             onClick={() => setIsOpen(!isOpen)}
           >
             {/* This is a close icon */}
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
-          <CustomLink to='/about2' className='py-2'>ABOUT</CustomLink>
-          <CustomLink to='/stories' className='py-2'>STORIES</CustomLink>
-          <CustomLink to='/misterras' className='py-2'>MIS TERRAS</CustomLink>
-          <CustomLink to='/laselfie' className='py-2'>LA SELFIE</CustomLink>
-          <CustomLink to='/education' className='py-2'>EDUCATION</CustomLink>
-          <a href='https://mailchi.mp/8bb2d79e8e70/sign-up' className='py-2'>CONTACT</a>
+          <CustomLink to='/about' className='py-2 font-avenir text-md hover:underline'>ABOUT</CustomLink>
+          <CustomLink to='/stories' className='py-2 font-avenir text-md hover:underline'>STORIES</CustomLink>
+          <CustomLink to='/misterras' className='py-2 font-avenir text-md hover:underline'>MIS TERRAS</CustomLink>
+          <CustomLink to='/laselfie' className='py-2 font-avenir text-md hover:underline'>LA SELFIE</CustomLink>
+          <CustomLink to='/education' className='py-2 font-avenir text-md hover:underline'>EDUCATION</CustomLink>
+          <div>
+            <a href='https://mailchi.mp/8bb2d79e8e70/sign-up' className='py-2 font-avenir text-md hover:underline'>CONTACT</a>
+          </div>
         </ul>
       )}
     </div>
